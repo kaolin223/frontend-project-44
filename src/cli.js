70c11a088f14623048d7e5489fba0411fbeb113a
kaolin223 <m-kuptsov@inbox.ru>
2022-12-08 19:55:46 +0300

    git add .

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 export default function greetings() {
     console.log('Welcome to the Brain Games!')
-    const nameUser = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${nameUser}!`);
+    const nameUser = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${nameUser}!`) 
 }
+
