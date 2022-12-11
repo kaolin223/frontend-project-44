596f5945060f4adc9c1fdbb2c7cd63c95db7ab37
kaolin223 <m-kuptsov@inbox.ru>
2022-12-11 17:23:40 +0300

    add update game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import logicGame from '../src/index.js';
 import calcRound from '../src/games/calc.js';
-import { phrases } from '../src/repeatingText';
+import { phrases } from '../src/repeatingText.js';
 
 const calcGame = () => logicGame(phrases.DESCRIPTION_CALC_GAME, calcRound);
 
